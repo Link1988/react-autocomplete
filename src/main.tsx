579b2cad8b4f06952d7518b15b024d4981e02d0c
Alejandro Hernández Ramírez <ahernandez@Alejandros-MacBook-Pro.local>
2024-12-31 20:58:37 -0600

    Add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { ErrorBoundary } from 'react-error-boundary';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+type FallbackProps = {
+  error: Error;
+}
+
+function fallback({ error }: FallbackProps) {
+  return (
+    <div role="alert">
+      <p>⚠️Something went wrong</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallbackRender={fallback}>
+        <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
